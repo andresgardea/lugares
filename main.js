136905a3540d8e06f6ee877ab0d9688e45d7ad54
unfotografomascuu <70489589+unfotografomascuu@users.noreply.github.com>
2023-12-28 00:12:08 -0600

    Movile Imperfecto

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     const slides = document.querySelectorAll(".slide");
+    const slidesMovile= document.querySelectorAll(".slide-movile");
+
     let currentSlideIndex = 0;
     let isAnimating = false;
     let currentTopValue = 0;
@@ -8,7 +10,13 @@ document.addEventListener("DOMContentLoaded", function () {
         { selector: ".prefix", delay: 0 },
         { selector: ".places", delay: 0.15 },
         { selector: ".year", delay: 0.3 },
-    ];
+    ];    
+
+    const elementsMovile = [
+        { selector: ".prefix-movile", delay: 0 },
+        { selector: ".places-movile", delay: 0.15 },
+        { selector: ".year-movile", delay: 0.3 },
+    ];    
 
     slides.forEach((slide, idx) => {
         if (idx !== 0) {
@@ -18,6 +26,16 @@ document.addEventListener("DOMContentLoaded", function () {
             });
         }
     });
+
+    slidesMovile.forEach((slide, idx) => {
+        if (idx !== 0) {
+            const img = slide.querySelector("img");
+            gsap.set(img, {
+                scale: 2, top: "2em"
+            });
+        }
+    });
+
 
     function showSlide(index) {
         if (isAnimating) return;
@@ -99,9 +117,89 @@ document.addEventListener("DOMContentLoaded", function () {
         );
     }
 
+    function showSlideMovile(index) {
+        if (isAnimating) return;
+        isAnimating = true;
+        const slideM = slidesMovile[index];
+        const img = slideM.querySelector("img");
+
+        currentTopValue -= 30;
+
+        elementsMovile.forEach((elem) => {
+            gsap.to(document.querySelector(elem.selector), {
+                y: `${currentTopValue}px`,
+                duration: 1,
+                ease: "power4.inOut",
+                delay: elem.delay,
+            });
+        });
+
+        gsap.to(img, {
+            scale: 1,
+            top: "0%",
+            duration: 1,
+            ease: "power3.inOut",
+        });
+
+        gsap.to(
+            slideM,
+            {
+                clipPath: "polygon(0 0%, 100% 0%, 100% 100%, 0 100%)",
+                duration: 1,
+                ease: "power4.inOut",
+                onComplete: () => {
+                    isAnimating = false;
+                },
+            },
+            "<"
+        );
+    }
+
+    function hideSlideMovile(index) {
+        if (isAnimating) return;
+        isAnimating = true;
+        const slideM = slidesMovile[index];
+        const img = slideM.querySelector("img");
+
+        currentTopValue += 30;
+
+        elementsMovile.forEach((elem) => {
+            gsap.to(document.querySelector(elem.selector), {
+                y: `${currentTopValue}px`,
+                duration: 1,
+                ease: "power4.inOut",
+                delay: elem.delay,
+            });
+        });
+
+        gsap.to(slideM, {
+            clipPath: "polygon(0 100%, 100% 100%, 100% 100%, 0 100%)",
+            duration: 1,
+            ease: "power4.inOut",
+        });
+
+        gsap.to(img, {
+            scale: 2,
+            top: "4em",
+            duration: 1,
+            ease: "power3.inOut"
+        });
+
+        gsap.to(slideM, {
+            clipPath: "polygon(0 100%, 100% 100%, 100% 100%, 0 100%)",
+            duration: 1,
+            ease: "power4.inOut",
+            onComplete: () => {
+                isAnimating = false;
+            },
+        },
+            "<"
+        );
+    }
     const desktopSize = window.matchMedia('(min-width: 601px)')
     const phoneSize = window.matchMedia('(max-width: 600px)')
 
+    //Funcionamiento en Escritorio (Desplazamiento)
     if (desktopSize.matches) {
         window.addEventListener("wheel", (e) => {
             if (isAnimating) return;
@@ -117,27 +215,26 @@ document.addEventListener("DOMContentLoaded", function () {
         console.log("Funciona en base a Scroll");
     }
 
+    //Funcionamiento en Movil (Click)
     if (phoneSize.matches) {
         const nextBtn = document.getElementById("next");
         const previousBtn = document.getElementById("previous");
 
         nextBtn.addEventListener('click', () => {
             if (isAnimating) return;
-            if (currentSlideIndex < slides.length - 1) {
-                showSlide(currentSlideIndex + 1);
+            if (currentSlideIndex < slidesMovile.length - 1) {
+                showSlideMovile(currentSlideIndex + 1);
                 currentSlideIndex++;
-            } else if (currentSlideIndex == slides.length) {
-                console.log("Acosta no sabe ensenar xD x2")
             }
         });
 
         previousBtn.addEventListener('click', () => {
             if (isAnimating) return;
             if (currentSlideIndex > 0) {
-                hideSlide(currentSlideIndex);
+                hideSlideMovile(currentSlideIndex);
                 currentSlideIndex--;
-            } else if (currentSlideIndex > slides.length - 1) {
-                hideSlide(currentSlideIndex);
+            } else if (currentSlideIndex > slidesMovile.length - 1) {
+                hideSlideMovile(currentSlideIndex);
                 currentSlideIndex--;
             }
         });
